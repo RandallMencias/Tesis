dbb72591cd0a38e1645f4876cd5b45ec10fe21fb
RandallMencias <randallmencias@gmail.com>
2024-09-15 00:35:04 -0500

    Limpieza de datos

---
+++ Manejo_DB.py
@@ -0,0 +1,11 @@
+#write a code to read the data from the json and display it in the console
+
+import json
+import pandas as pd
+with open('Resources/SeisBenchV1_v1_1.json') as file:
+    data = json.load(file)
+
+#converty to pandas dataframe
+
+df = pd.DataFrame(data)
+print(df.head())
